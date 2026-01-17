de330dfef8ee81968af64fd7123a21a87531c9a6
claverlo <leopac84@icloud.com>
2026-01-16 20:47:45 -0800

    Service form validation with jQuery

--- scripts/services.js
+++ scripts/services.js
@@ -1,28 +1,33 @@
 $("#servicesForm").submit(function (event) {
 
   event.preventDefault();
-  //console.log(event);
-$("#serviceName").css("border", "");
-$("#serviceDescription").removeAttr("style");
-$("#servicePrice").removeAttr("style");
-
+  $("#serviceName").css("border", "");
+  $("#serviceDescription").removeAttr("style");
+  $("#servicePrice").removeAttr("style");
 
   const name = $("#serviceName").val().trim();
   const description = $("#serviceDescription").val().trim();
   const price = $("#servicePrice").val();
 
+  let isValid = true;
 
   if (!name) {
     $("#serviceName").css("border", "3px solid red");
+    isValid = false;
   }
   if (!description) {
     $("#serviceDescription").css("border", "3px solid red");
+    isValid = false;
   }
   if (!price || price <= 0) {
     $("#servicePrice").css("border", "3px solid red");
+    isValid = false;
   } else {
  
   }
 
+  if (isValid) {
+    new Service(name, description, price);
     this.reset();
+  }
 });
